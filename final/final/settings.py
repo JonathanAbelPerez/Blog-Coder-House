f504f9efcb5066c495a404e02b62122da7c9e1d4
Jonathan Perez <jonathanabelerez@gmail.com>
2022-06-25 03:22:46 -0300

    cambios visuales en html

--- final/final/settings.py
+++ final/final/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'cuentas',
     'posts',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
--- final/posts/models.py
+++ final/posts/models.py
@@ -2,12 +2,14 @@ from audioop import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Post(models.Model):
     titulo = models.CharField(max_length= 255, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    cuerpo= models.TextField()
+    cuerpo = RichTextField(blank = True, null= True)
+    #cuerpo= models.TextField()
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
